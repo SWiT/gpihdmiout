ba255d082c06f395dabc7baefe85478aa7d5db79
Matt Switlik <switlik@oakland.edu>
2021-11-11 16:49:42 -0500

    Raspberry Pi Zero 2 W support.

--- detect.sh
+++ detect.sh
@@ -7,13 +7,25 @@ TVSTATUS=$(tvservice -s)
 # Patched:
 #       state 0x400000 [LCD], 320x240 @ 0.00Hz, progressive
 # Unpatched, no HDMI at boot:
+#   RPi Zero 1
 #       state 0x40001 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
+#   RPi Zero 2
+#       state 0x40000 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
 # Unpatched, no HDMI at boot, then HDMI inserted
+#   RPi Zero 1
 #       state 0x40002 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
+#   RPi Zero 2
+#       state 0x40000 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
 # Unpatched, HDMI at boot:
+#   RPi Zero 1
 #       state 0x12000a [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
+#   RPi Zero 2
+#       state 0xa [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
 # Unpatched, HDMI at boot, then HDMI removed:
+#   RPi Zero 1
 #       state 0x120009 [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
+#   RPi Zero 2
+#       state 0x9 [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
 
 echo >> $LOG
 date >> $LOG
@@ -22,7 +34,7 @@ TVCODE=$(echo $TVSTATUS | cut -c7-14)
 echo "\"$TVCODE\"" >> $LOG
 if [ $TVCODE == "0x400000" ]; then
     echo "GPi LCD Enabled. Continue booting." >> $LOG
-elif [ $TVCODE == "0x40001" ]; then
+elif [ $TVCODE == "0x40000" ] || [ $TVCODE == "0x40001" ]; then
     echo "No HDMI connected. Enable GPi LCD, and reboot." >> $LOG
     $HDMIDISABLE >> $LOG
     reboot
