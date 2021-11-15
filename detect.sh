61226d15d9fc97e471fbc4cb63e147097335a8b2
Matt Switlik <switlik@oakland.edu>
2021-11-14 20:51:44 -0500

    Updated TV state codes for zero 1. Not sure when they changed.

--- detect.sh
+++ detect.sh
@@ -6,34 +6,22 @@ HDMIDISABLE="/home/pi/gpihdmiout/disable.sh -y"
 # Run the tvservice command and store the output for parsing.
 TVSTATUS=$(tvservice -s)
     
+# Kernel v5.10.63+
 # Patched:
 #       state 0x400000 [LCD], 320x240 @ 0.00Hz, progressive
 # Unpatched, no HDMI at boot:
-#   RPi Zero 1
-#       state 0x40001 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
-#   RPi Zero 2
-#       state 0x40000 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
-# Unpatched, no HDMI at boot, then HDMI inserted
-#   RPi Zero 1
-#       state 0x40002 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
-#   RPi Zero 2
 #       state 0x40000 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
 # Unpatched, HDMI at boot:
-#   RPi Zero 1
-#       state 0x12000a [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
-#   RPi Zero 2
 #       state 0xa [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
+#       state 0xa [HDMI DMT (85) RGB full 16:9], 1280x720 @ 60.00Hz, progressive
 # Unpatched, HDMI at boot, then HDMI removed:
-#   RPi Zero 1
-#       state 0x120009 [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
-#   RPi Zero 2
 #       state 0x9 [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
 
 # Parse the tvservice command output to get just the TV state code.
 TVCODE=$(echo $TVSTATUS | cut -c7-14)
 if [ $TVCODE == "0x400000" ]; then
     echo "GPi CASE LCD Enabled. Continue booting."
-elif [ $TVCODE == "0x40000" ] || [ $TVCODE == "0x40001" ]; then
+elif [ $TVCODE == "0x40000" ] then
     echo "No HDMI connected. Enable GPi CASE LCD, and reboot."
     $HDMIDISABLE
     reboot
